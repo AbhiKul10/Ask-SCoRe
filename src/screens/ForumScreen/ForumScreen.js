172b0ebc25c695906506f00c4e1769e4e3535eee
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-06-26 19:39:42 +0530

    Implemented Forum Screen

--- src/screens/ForumScreen/ForumScreen.js
+++ src/screens/ForumScreen/ForumScreen.js
@@ -6,41 +6,64 @@ import { LOGO } from '../../config/styles.js';
 import styles from './styles';
 
 class ForumScreen extends Component {
-    render() {
-        return (
-            <SafeAreaView>
-              <View>
-                <View style={{marginBottom: 15}}>
-                    <View style={styles.HeaderStyle1}>
-                        <Image source={LOGO} style={styles.ImageView} />
-                        <Text style={styles.HeaderStyle}>Forums</Text>
-                        <TouchableOpacity style={styles.AskButtonStyle} onPress={() => {
-                            this.props.navigation.navigate("AddChat")
-                        }}>
-                            <Text style = {{color: "black", fontWeight: '500', textAlign: 'center', fontSize: 16 }}>+</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.DividerView} />
-                    <View style={styles.DividerView} />
-                </View>
+  state = {dialogvisible: false, forumtext: '', roomArray : []};
 
-                <View>
-                  <TouchableOpacity onPress={() => this.props.navigation.push("Chat")}>
-                    <View>
-                        <View style={{flexDirection:'row', marginLeft:10}}>
-                            <Image source={LOGO} style={styles.ImageView1}/>
-                            <Text style={{alignSelf:'center', fontSize:16, marginHorizontal:15, marginBottom:2}}>
-                                GSoC
-                            </Text>
-                        </View>
-                    </View>
-                  </TouchableOpacity>
-                <View style={styles.DividerView1} />
-              </View>
+  componentDidMount(){
+    const {roomArray} = this.state  
+    firebase.database().ref().on('value', (snapshot) => {
+      newroomArray = []
+      snapshot.forEach(element => {
+          newroomArray.push(element.key);
+          this.setState({roomArray : newroomArray})
+          console.log(roomArray)
+      });
+    })  
+  }
+
+  render() {
+    const {roomArray} = this.state
+    return (
+      <SafeAreaView>
+        <View>
+          <View style={{marginBottom: 15}}>
+            <View style={styles.HeaderStyle1}>
+                <Image source={LOGO} style={styles.ImageView} />
+                <Text style={styles.HeaderStyle}>Forums</Text>
+                <TouchableOpacity style={styles.AskButtonStyle} onPress={() => {
+                    this.props.navigation.navigate("AddChat")
+                }}>
+                    <Text style = {{color: "black", fontWeight: '500', textAlign: 'center', fontSize: 16 }}>+</Text>
+                </TouchableOpacity>
             </View>
-          </SafeAreaView>
-        );
-    }
+            <View style={styles.DividerView} />
+            <View style={styles.DividerView} />
+          </View>
+          <FlatList
+            data={roomArray}
+            renderItem = {({item}) => {
+              return (
+                  <View>
+                    <TouchableOpacity onPress={() => this.props.navigation.push("Chat", {
+                    data: item})}>
+                      <View>
+                          <View style={{flexDirection:'row', marginLeft:10}}>
+                              <Image source={LOGO} style={styles.ImageView1}/>
+                              <Text style={{alignSelf:'center', fontSize:16, marginHorizontal:15, marginBottom:2}}>
+                                  {item}
+                              </Text>
+                          </View>
+                      </View>
+                    </TouchableOpacity>
+                    <View style={styles.DividerView1} />
+                  </View>
+              )
+            }}
+            keyExtractor={(item, index) => 'key'+index}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 export default ForumScreen;
