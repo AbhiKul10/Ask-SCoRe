619ad1da2f0469df88128c2c02167bff0de978d3
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-06-26 22:12:44 +0530

    designed and implemented Chat Screen

--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -1,12 +1,115 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import { Component } from 'react';
+import { View, Text, SafeAreaView, Image, TouchableOpacity, TextInput, KeyboardAvoidingView, FlatList } from 'react-native';
+import { firebase } from '../../config/config';
+import moment from 'moment';
+import styles from './styles.js';
 
 class ChatScreen extends Component{
-    render(){
+
+    state = {messageList : '', data: ''}
+
+    UNSAFE_componentWillMount() {
+        const data = this.props.navigation.getParam('data');
+        this.setState({
+            data
+        }, this.renderData);
+    }
+
+    writeUserData(message){
+        var timeDate = moment();
+        firebase.firestore().collection("users").doc(firebase.auth().currentUser.uid).get().then((doc) => {
+            firebase.database().ref(this.state.data + '/').push({
+                name: doc.data().fullName,
+                message,
+                creation:  new Date().toUTCString(),
+                messageTime: timeDate.format('lll'),
+                imageURL : doc.data().downloadURL
+            }).then((data) => {
+                this.setState({message: ''})
+            }).catch((error) => {
+                alert(error)
+            })
+        })
+    }
+
+    renderData(){
+        const {messageList} = this.state
+        firebase.database().ref(this.state.data + '/').on('value', (snapshot) => {
+            var messageArray = []
+            this.setState({messageList: Object.values(snapshot.val())})
+        })
+    }
+
+    render() {  
+        const { messageList } = this.state  
         return(
-            <View>
-                <Text>Chat Screen</Text>
-            </View>
+            <>
+            <SafeAreaView style={{flex:0, backgroundColor: "#51AD28"}} />
+            <SafeAreaView style={{flex:1}}>
+               <View style={{flex:1}}>
+                <View style={{flex:1}}>
+                    <View style={{marginBottom: 5}}>
+                        <View style={styles.DividerView} />
+                    </View>
+                    <FlatList
+                    ref={ref => {this.FlatListRef = ref}}
+                    onContentSizeChange={() => this.FlatListRef.scrollToEnd()} // scroll it
+                    data={messageList}
+                    renderItem = {({item}) => {
+                        return (
+                            <View>
+                                <View style={{marginLeft:10, marginRight: 48, marginVertical: 7}}>
+                                    <View style={{flexDirection: 'column', flex:1}}>
+                                        <View style={{flexDirection: 'row'}}>
+                                            <Image source={{uri: item.imageURL}} style={styles.ImageView1} />
+                                            <View>
+                                                <View style={{flexDirection:'column', marginLeft: 5}}>
+                                                    <View style={{flexDirection: 'row', marginBottom: 5}}>
+                                                        <Text style={{fontWeight: "500", marginRight:7}}>{item.name}</Text>
+                                                        <Text style={{color : 'gray', fontSize: 10, marginTop: 2}}>{item.messageTime}</Text>
+                                                    </View>
+                                                    <Text style={{fontSize: 13, fontWeight: "300"}}>{item.message}</Text>
+                                                </View>
+                                            </View>
+                                        </View>
+                                    </View>
+                                </View>
+                        <View style={styles.DividerView1} />
+                        </View>
+                        )
+                    }}
+                    keyExtractor={(item, index) => 'key'+index}
+                    />
+                    </View>  
+                    </View>  
+                <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : null}
+                keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+                >    
+                <View style={{flexDirection: 'row', height:35}}>
+                    <View style={{borderColor: "gray", borderWidth: 2, borderRadius: 20, marginHorizontal:10, width: "77%", marginRight:15}}>
+                        <TextInput 
+                        style={{paddingHorizontal:10 ,flex:1, backgroundColor:'white', borderRadius: 20}}
+                        value={this.state.message}
+                        placeholder="Enter Message here"
+                        onChangeText={(message) =>  this.setState({message})}
+                        />
+                    </View>
+
+                    <View>
+                        <TouchableOpacity 
+                        style={styles.AskButtonStyle1}
+                        onPress = {() => this.writeUserData(this.state.message)}
+                        value = {this.state.message}
+                        >
+                            <Text style = {{color: "black", fontWeight: '500', textAlign: 'center', fontSize: 16 }}>SEND</Text>
+                        </TouchableOpacity>
+                    </View>
+                    </View>
+                </KeyboardAvoidingView>       
+            </SafeAreaView>
+            </>
         )
     }
 }
---
+++ src/screens/ChatScreen/styles.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    HeaderStyle1 : {
+        flexDirection: 'row',
+    },
+
+    ImageView : {
+        width: 55,
+        height: 55,
+        marginHorizontal: 5
+    },
+
+    HeaderStyle : {
+        fontSize: 30,
+        flex: 1,
+        marginLeft: 10, 
+        marginTop: 5
+    },
+
+    AskButtonStyle : {
+        width: 60,
+        height: 30,
+        backgroundColor: "#9cd683",
+        borderRadius: 10,
+        right: 10,
+        justifyContent: 'center',
+        marginTop: 7
+    },
+
+    AskButtonStyle1 : {
+        width: 60,
+        height: 35,
+        backgroundColor: "#9cd683",
+        borderRadius: 10,
+        right: 10,
+        justifyContent: 'center',  
+    },
+
+    DividerView : {
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        marginBottom:3,
+    },
+
+    DividerView1 : {
+        borderBottomColor: '#e6e6e6',
+        borderBottomWidth: 1,
+        marginTop:2,
+        marginBottom:5
+    },
+
+    ImageView1 : {
+        height: 28,
+        width: 28,
+        borderRadius: 5,
+        backgroundColor: '#d4d4d4',
+        marginRight: 7,
+    },
+})
