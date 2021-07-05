6f7b61a9c0004bede7c488fa00f0f7b03354c538
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-07-05 18:46:05 +0530

    removed credentials

--- src/config/config.js
+++ src/config/config.js
@@ -6,15 +6,14 @@ import "@firebase/firestore";
 import "@firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVSz7SBgykflS-1or_2cL2NO_9zKPcgnM",
-  authDomain: "trydesign-f5380.firebaseapp.com",
-  databaseURL:
-    "https://trydesign-f5380-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "trydesign-f5380",
-  storageBucket: "trydesign-f5380.appspot.com",
-  messagingSenderId: "735297362112",
-  appId: "1:735297362112:web:3bc069313763f5e942f525",
-  measurementId: "G-MTTJ7H8SEY",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 if (!firebase.apps.length) {
