258af6f89d56d1a5ee599e6a364627536b60655f
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-07-08 23:54:16 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,6 @@ import "@firebase/firestore";
 import "@firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   apiKey: "",
   authDomain: "",
   projectId: "",
@@ -14,21 +13,13 @@ const firebaseConfig = {
   messagingSenderId: "",
   appId: "",
   measurementId: "",
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// firebase.firestore().settings({ experimentalForceLongPolling: true });
+firebase.firestore().settings({experimentalForceLongPolling: true});
 
 export {firebase};
 
